b100b2040bc973f0a0ccf0e967a60cdce8f7e513
Tj <devultj@gmail.com>
2021-10-12 20:18:40 +0100

    Bunch more HUD work. MiniMap boilerplate.

---
+++ code/Systems/UI/Hud/MiniMap/MiniMap.cs
@@ -0,0 +1,20 @@
+﻿
+using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+using System;
+using System.Threading.Tasks;
+
+namespace Conquest
+{
+	[UseTemplate( "systems/ui/hud/minimap/minimap.html" )]
+	public class MiniMap : Panel
+	{
+		public Panel CapturePointsPanel { get; set; }
+		public Panel MiniMapPanel { get; set; }
+		public MiniMap()
+		{
+
+		}
+	}
+}
