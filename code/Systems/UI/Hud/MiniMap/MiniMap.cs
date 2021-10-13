e10c30d3b431fb0a5e215887eb4e09b0e86bfd5d
Tj <devultj@gmail.com>
2021-10-13 16:39:24 +0100

    Populate minimap with real capture points
    Shit code, replace later

--- code/Systems/UI/Hud/MiniMap/MiniMap.cs
+++ code/Systems/UI/Hud/MiniMap/MiniMap.cs
@@ -3,6 +3,8 @@ using Sandbox;
 using Sandbox.UI;
 using Sandbox.UI.Construct;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Conquest
@@ -12,9 +14,34 @@ namespace Conquest
 	{
 		public Panel CapturePointsPanel { get; set; }
 		public Panel MiniMapPanel { get; set; }
+
+		public List<Panel> CapturePointPanels { get; set; } = new();
+
 		public MiniMap()
 		{
+			foreach( var capturePoint in Entity.All.OfType<CapturePointEntity>() )
+			{
+				var panel = CapturePointsPanel.AddChild<Panel>( "capturepoint" );
+				var label = panel.AddChild<Label>();
+				label.Text = capturePoint.Identity;
 
+				CapturePointPanels.Add( panel );
+			}
+		}
+
+		public override void Tick()
+		{
+			base.Tick();
+
+			var localPlayer = Local.Pawn as Player;
+
+			int i = 0;
+			foreach( var capturePoint in Entity.All.OfType<CapturePointEntity>() )
+			{
+				var friendState = TeamSystem.GetFriendState( localPlayer.Team, capturePoint.Team );
+				CapturePointPanels[i].SetClass( "friendly", friendState == TeamSystem.FriendlyStatus.Friendly );
+				CapturePointPanels[i].SetClass( "enemy", friendState == TeamSystem.FriendlyStatus.Hostile );
+			}
 		}
 	}
 }
