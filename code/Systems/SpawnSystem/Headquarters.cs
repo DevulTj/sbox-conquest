9ed1e12c6001bb1260887d7b7c96c4fc3162d5a5
Tj <devultj@gmail.com>
2021-10-20 15:36:02 +0100

    Show Headquarters as minimap markers

--- code/Systems/SpawnSystem/Headquarters.cs
+++ code/Systems/SpawnSystem/Headquarters.cs
@@ -9,13 +9,36 @@ namespace Conquest
 	[Library( "conquest_headquarters" )]
 	[Hammer.EditorModel( "models/editor/playerstart.vmdl", FixedBounds = true )]
 	[Hammer.Solid]
-	public partial class Headquarters : Entity, IGameStateAddressable
+	public partial class Headquarters : Entity, IGameStateAddressable, IMiniMapEntity
 	{
 		[Net, Property] public TeamSystem.Team Team { get; set; }
 
+		public override void Spawn()
+		{
+			base.Spawn();
+
+			Transmit = TransmitType.Always;
+		}
+
 		void IGameStateAddressable.ResetState()
 		{
-			
+		}
+
+		string IMiniMapEntity.GetMainClass() => "capturepoint";
+		bool IMiniMapEntity.Update( ref MiniMapDotBuilder info )
+		{
+			if ( !this.IsValid() )
+				return false;
+
+			info.Text = "HQ";
+			info.Position = Position;
+
+			var friendState = TeamSystem.GetFriendState( Team, TeamSystem.MyTeam );
+			info.Classes["friendly"] = friendState == TeamSystem.FriendlyStatus.Friendly;
+			info.Classes["enemy"] = friendState == TeamSystem.FriendlyStatus.Hostile;
+			info.Classes["headquarters"] = true;
+
+			return true;
 		}
 	}
 }
