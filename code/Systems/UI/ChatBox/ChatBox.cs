95633d07e33108118e44aa0c3e66fdf997771b9a
Tj <devultj@gmail.com>
2021-10-20 15:11:09 +0100

    Added basic ChatBox

---
+++ code/Systems/UI/ChatBox/ChatBox.cs
@@ -0,0 +1,134 @@
+
+using Sandbox;
+using Sandbox.Hooks;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+using System;
+using System.Linq;
+
+namespace Conquest
+{
+	[Library( "ConquestChatBox" )]
+	public partial class ChatBox : Panel
+	{
+		static ChatBox Current;
+
+		public Panel Canvas { get; protected set; }
+		public TextEntry Input { get; protected set; }
+
+		public ChatBox()
+		{
+			Current = this;
+
+			StyleSheet.Load( "/systems/ui/chatbox/chatbox.scss" );
+
+			Canvas = Add.Panel( "chat_canvas" );
+
+			Input = Add.TextEntry( "" );
+			Input.AddEventListener( "onsubmit", () => Submit() );
+			Input.AddEventListener( "onblur", () => Close() );
+			Input.AcceptsFocus = true;
+			Input.AllowEmojiReplace = true;
+
+			Hooks.Chat.OnOpenChat += Open;
+		}
+
+		void Open()
+		{
+			AddClass( "open" );
+			Input.Focus();
+		}
+
+		void Close()
+		{
+			RemoveClass( "open" );
+			Input.Blur();
+		}
+
+		void Submit()
+		{
+			Close();
+
+			var msg = Input.Text.Trim();
+			Input.Text = "";
+
+			if ( string.IsNullOrWhiteSpace( msg ) )
+				return;
+
+			Say( msg );
+		}
+
+
+		public void AddEntry( string name, string message, string avatar, TeamSystem.Team team = TeamSystem.Team.BLUFOR )
+		{
+			var e = Canvas.AddChild<ChatEntry>();
+			//e.SetFirstSibling();
+			e.Message.Text = message;
+			e.NameLabel.Text = name;
+
+			e.TimestampLabel.Text = e.Time.ToString( "h:mm tt" );
+
+			e.NameLabel.SetClass( "friendly", TeamSystem.IsFriendly( TeamSystem.MyTeam, team ) );
+
+			e.Avatar.SetTexture( avatar );
+
+			e.SetClass( "noname", string.IsNullOrEmpty( name ) );
+			e.SetClass( "noavatar", string.IsNullOrEmpty( avatar ) );
+		}
+
+
+		[ClientCmd( "chat_add", CanBeCalledFromServer = true )]
+		public static void AddChatEntry( string name, string message, string avatar = null, TeamSystem.Team team = TeamSystem.Team.BLUFOR )
+		{
+			Current?.AddEntry( name, message, avatar, team );
+
+			// Only log clientside if we're not the listen server host
+			if ( !Global.IsListenServer )
+			{
+				Log.Info( $"{name}: {message}" );
+			}
+		}
+
+		[ClientCmd( "chat_addinfo", CanBeCalledFromServer = true )]
+		public static void AddInformation( string message, string avatar = null )
+		{
+			Current?.AddEntry( null, message, avatar );
+		}
+
+
+		public static Client Caller = null;
+		[ServerCmd( "say" )]
+		public static void Say( string message )
+		{
+			Assert.NotNull( ConsoleSystem.Caller );
+
+			// todo - reject more stuff
+			if ( message.Contains( '\n' ) || message.Contains( '\r' ) )
+				return;
+
+			var player = ConsoleSystem.Caller.Pawn as Player;
+
+			Event.Run( GameEvent.Server.OnChatMessage, ConsoleSystem.Caller, message );
+
+			Log.Info( $"{ConsoleSystem.Caller}: {message}" );
+
+			AddChatEntry( To.Everyone, ConsoleSystem.Caller.Name, message, $"avatar:{ConsoleSystem.Caller.SteamId}", player.Team );
+		}
+
+	}
+}
+
+namespace Conquest.Hooks
+{
+	public static partial class Chat
+	{
+		public static event Action OnOpenChat;
+
+		[ClientCmd( "openchat" )]
+		internal static void MessageMode()
+		{
+			OnOpenChat?.Invoke();
+		}
+
+	}
+}
---
+++ code/Systems/UI/ChatBox/ChatEntry.cs
@@ -0,0 +1,37 @@
+﻿using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+using System;
+
+namespace Conquest
+{
+	public partial class ChatEntry : Panel
+	{
+		public DateTime Time { get; set; } = DateTime.Now;
+
+		public Label TimestampLabel { get; internal set; }
+		public Label NameLabel { get; internal set; }
+		public Label Message { get; internal set; }
+		public Image Avatar { get; internal set; }
+
+		public RealTimeSince TimeSinceBorn = 0;
+
+		public ChatEntry()
+		{
+			Avatar = Add.Image();
+			TimestampLabel = Add.Label( "Timestamp", "time" );
+			NameLabel = Add.Label( "Name", "name" );
+			Message = Add.Label( "Message", "message" );
+		}
+
+		public override void Tick() 
+		{
+			base.Tick();
+
+			if ( TimeSinceBorn > 10 ) 
+			{ 
+				Delete();
+			}
+		}
+	}
+}
