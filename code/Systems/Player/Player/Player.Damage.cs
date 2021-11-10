9e171e590c000410cb054837326a1f2befc643a3
Tj <devultj@gmail.com>
2021-11-10 15:46:50 +0000

    Tell player if their damage was a headshot

--- code/Systems/Player/Player/Player.Damage.cs
+++ code/Systems/Player/Player/Player.Damage.cs
@@ -12,9 +12,10 @@ namespace Conquest
 		{
 			LastDamage = info;
 
+			bool isHeadshot = info.HitboxIndex == 5;
 			// hack - hitbox 0 is head
 			// we should be able to get this from somewhere
-			if ( info.HitboxIndex == 5 )
+			if ( isHeadshot )
 			{
 				info.Damage *= 2.0f;
 
@@ -27,7 +28,7 @@ namespace Conquest
 			if ( info.Attacker is Player attacker && attacker != this )
 			{
 				// Note - sending this only to the attacker!
-				attacker.DidDamage( To.Single( attacker ), info.Position, info.Damage, Health.LerpInverse( 100, 0 ) );
+				attacker.DidDamage( To.Single( attacker ), info.Position, info.Damage, Health.LerpInverse( 100, 0 ), isHeadshot );
 
 				TookDamage( To.Single( this ), info.Weapon.IsValid() ? info.Weapon.Position : info.Attacker.Position );
 			}
@@ -40,12 +41,12 @@ namespace Conquest
 		}
 
 		[ClientRpc]
-		public void DidDamage( Vector3 pos, float amount, float healthinv )
+		public void DidDamage( Vector3 pos, float amount, float healthinv, bool isHeadshot = false )
 		{
 			Sound.FromScreen( "dm.ui_attacker" )
 				.SetPitch( 1 + healthinv * 1 );
 
-			//	HitIndicator.Current?.OnHit( pos, amount );
+			HitIndicator.Current?.OnHit( pos, amount, healthinv == 1, isHeadshot );
 		}
 
 		[ClientRpc]
