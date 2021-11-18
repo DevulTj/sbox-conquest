30377c6f2a65bdb7001f5cdeb11188e1506509f3
Tj <devultj@gmail.com>
2021-11-18 15:55:05 +0000

    Added Conquest.Net.To which has shortcuts for BLUFOR, OPFOR and specified Team

---
+++ code/Extensions/To.cs
@@ -0,0 +1,14 @@
+
+using Sandbox;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Conquest.Net;
+
+public class To
+{
+	public static IEnumerable<Client> BLUFOR => Team( Conquest.Team.BLUFOR );
+	public static IEnumerable<Client> OPFOR => Team( Conquest.Team.BLUFOR );
+
+	public static IEnumerable<Client> Team( Team team ) =>  Client.All.Where( x => TeamSystem.GetTeam( x ) == team );
+}
