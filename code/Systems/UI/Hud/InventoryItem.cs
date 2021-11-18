39e1cb8803b88d8db76e86150fd357f728b6648d
Tj <devultj@gmail.com>
2021-11-18 15:03:46 +0000

    Update InventoryItem.cs

--- code/Systems/UI/Hud/InventoryItem.cs
+++ code/Systems/UI/Hud/InventoryItem.cs
@@ -47,7 +47,6 @@ public class InventoryItem : Panel
 		{
 			Weapon = weaponFromSlot;
 
-			Log.Info( Weapon.WeaponInfo?.LoadoutIcon ?? "no icon" );
 			Icon?.SetTexture( Weapon.WeaponInfo?.LoadoutIcon );
 		}
 
