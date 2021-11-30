9abf94ccd2b9a542302adeaa9937f9cb0612e216
Tj <devultj@gmail.com>
2021-11-30 16:29:09 +0000

    Can't deploy in the StartCountdown stage.

--- code/Systems/GameModes/Stronghold/States/StartCountdown.cs
+++ code/Systems/GameModes/Stronghold/States/StartCountdown.cs
@@ -8,6 +8,7 @@ public partial class StartCountdownGameState : GameState
 	[ConVar.Replicated( "conquest_stronghold_startcountdown" )]
 	public static int StartCountdown { get; set; } = 10;
 
+	public override bool CanDeploy => false;
 	public override int TimeLimit => StartCountdown;
 	public override string ToString() => "GameState[Stronghold][StartCountdown]";
 
