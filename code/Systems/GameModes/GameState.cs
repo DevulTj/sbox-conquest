63517781e7e7832bfd7109590390b9fd0c0ca3b1
Tj <devultj@gmail.com>
2021-11-30 16:22:50 +0000

    Added TimeLimit property to base GameState

--- code/Systems/GameModes/GameState.cs
+++ code/Systems/GameModes/GameState.cs
@@ -22,11 +22,22 @@ public partial class GameState : BaseNetworkable
 	[Net, Predicted] public TimeSince TimeSinceStart { get; set; } = 0;
 	[Net, Predicted] public TimeSince TimeSinceEnd { get; set; } = 0;
 
+	public virtual int TimeLimit => 0;
+	[Net, Predicted] public bool HasTimeLimitReached { get; set; } = false;
+
 	public override string ToString() => "GameStateBase";
 
 	public virtual void Tick( float delta )
 	{
-		// 
+		if ( !HasTimeLimitReached && TimeLimit != 0 && TimeSinceStart >= TimeLimit )
+		{
+			OnTimeLimitReached();
+		}
+	}
+
+	protected virtual void OnTimeLimitReached()
+	{
+		HasTimeLimitReached = true;
 	}
 
 	/// <summary>
