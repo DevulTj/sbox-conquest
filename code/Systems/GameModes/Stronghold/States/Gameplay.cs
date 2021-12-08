a61380c0d03a8d33086c2634fbd83f99ec7d9f32
Tj <devultj@gmail.com>
2021-12-08 13:25:08 +0000

    Added time limit to gameplay (5 mins as default)

--- code/Systems/GameModes/Stronghold/States/Gameplay.cs
+++ code/Systems/GameModes/Stronghold/States/Gameplay.cs
@@ -5,11 +5,29 @@ namespace Conquest.Stronghold;
 
 public partial class GameplayGameState : GameState
 {
+	[ConVar.Replicated( "conquest_stronghold_gamelength" )]
+	public static int GameLength { get; set; } = 10;
+
 	public override string Identifier => "Gameplay";
+	public override int TimeLimit => GameLength;
+	public override string DisplayText => FormattedTimeRemaining;
 
 	// When scores hit zero, set the winner.
 	public override void OnScoreHitZero( Team winner )
 	{
+		var winnerState = new WinnerDecidedGameState();
+		winnerState.WinningTeam = winner;
+
+		GameMode.SetGameState( winnerState );
+	}
+
+	protected override void OnTimeLimitReached()
+	{
+		base.OnTimeLimitReached();
+
+		// @TODO: Move scoring system into gamemode
+		var winner = Game.Current?.Scores.GetHighestTeam() ?? Team.Unassigned;
+
 		var winnerState = new WinnerDecidedGameState();
 		winnerState.WinningTeam = winner;
 
