2f3327e94bc8ca7c6823a6bc7b42efd4cdda356d
Tj <devultj@gmail.com>
2021-12-08 14:36:14 +0000

    Some shit placeholder code to test out the showcase camera

--- code/Systems/GameModes/Stronghold/States/Showcase.cs
+++ code/Systems/GameModes/Stronghold/States/Showcase.cs
@@ -1,18 +1,57 @@
 
 using Sandbox;
+using System;
+using System.Collections.Generic;
 
 namespace Conquest.Stronghold;
 
 public partial class ShowcaseCamera : Camera
 {
+	public ShowcaseCamera()
+	{
+		CurrentPos = StartingCameraPosition;
+		TargetPos = StartingCameraPosition;
+	}
+
 	public virtual Vector3 StartingCameraPosition => new Vector3( 1162.81f, -216.94f, 109.65f );
 	public virtual Angles StartingCameraAngles => new Angles( 11.40f, -25.65f, 0f );
 
+	public List<Vector3> FocusPoints => new List<Vector3>() { StartingCameraPosition + new Vector3( 10, 10, 0 ), StartingCameraPosition + new Vector3( 20, 20, 0 ), StartingCameraPosition + new Vector3( 30, 30, 0 ), StartingCameraPosition + new Vector3( 40, 40, 0 ) };
+
+	public int FocusPointLength = 2;
+
+
+	private Vector3 TargetPos;
+	private Vector3 CurrentPos;
+	private Angles CurrentAngles;
+
+	private TimeSince SinceSwitched = 0;
+	private int Index = 0;
+
 	public override void Update()
 	{
-		Position = StartingCameraPosition;
+		CurrentPos = CurrentPos.LerpTo( TargetPos, Time.Delta * 10f );
+
+		Position = CurrentPos;
 		Rotation = StartingCameraAngles.ToRotation();
+
 		FieldOfView = 60f;
+
+		if ( SinceSwitched > FocusPointLength )
+		{
+			FocusNextPoint();
+		}
+	}
+
+	private void FocusNextPoint()
+	{
+		Index++;
+
+		if ( Index > FocusPoints.Count - 1 )
+			return;
+
+		TargetPos = FocusPoints[Index];
+		SinceSwitched = 0;
 	}
 }
 
